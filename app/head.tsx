dfb32803b7662d70098178b7d987503fefdfabfd
Tommy Barvåg <tommy@barvaag.com>
2023-02-09 12:44:04 +0100

    Add head to render og:image tag

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+import { HeadOgImageUrlCalendar } from "./head-image-url";
+
+export default function Head({ params }) {
+  return (
+    <>
+      <HeadOgImageUrlCalendar params={params} />
+    </>
+  );
+}
---
+++ app/year/[year]/head.tsx
@@ -0,0 +1,9 @@
+import { HeadOgImageUrlCalendar } from "@/app/head-image-url";
+
+export default function Head({ params }) {
+  return (
+    <>
+      <HeadOgImageUrlCalendar params={params} />
+    </>
+  );
+}
